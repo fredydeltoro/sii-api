3b21305b7d0b4e1cb586ee6f58c54f9a7a6de91d
Freddy Monroy <fredydeltoro@gmail.com>
2021-09-19 13:43:52 -0500

    Husky listo

--- src/app/app.js
+++ src/app/app.js
@@ -3,10 +3,6 @@ const cors = require('@koa/cors');
 
 const app = new Koa();
 
-console.log(
-  'kakakakakaskdjakdjasdjfasdfasdfadsfasdfasdfasdfasdfasdfasdfasdfasdfas',
-);
-
 app.use(cors());
 
 module.exports = app;
