4e2de9746974f292ef58b4b5612c98295d4caeb4
Freddy Monroy <fredydeltoro@gmail.com>
2021-09-19 13:43:32 -0500

    Husky prueba

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,10 @@ const cors = require('@koa/cors');
 
 const app = new Koa();
 
+console.log(
+  'kakakakakaskdjakdjasdjfasdfasdfadsfasdfasdfasdfasdfasdfasdfasdfasdfas',
+);
+
 app.use(cors());
 
 module.exports = app;
