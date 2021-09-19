0e0eb6d98095d4f6bdba064ae5cc6f34b91031e3
Freddy Monroy <fredydeltoro@gmail.com>
2021-09-19 12:57:51 -0500

    Lint staged

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,6 @@ const health = require('./health');
 module.exports = (app) => {
   const router = new Router();
 
-  console.log('algo');
-
   router.use('/health', health(Router));
 
   app.use(router.routes());
