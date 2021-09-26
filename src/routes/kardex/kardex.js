70ca6433a3b90005527daca6dbaba04864b285db
Freddy Monroy <fredydeltoro@gmail.com>
2021-09-25 19:32:22 -0500

    Obtener el kardex del sii

--- src/routes/kardex/kardex.js
+++ src/routes/kardex/kardex.js
@@ -6,8 +6,16 @@ const subjectProgress = require('../../models/subjectProgress.js');
 module.exports = (Router) => {
   const router = new Router();
 
-  router.get('/', ({ response }) => {
+  router.get('/', async ({ response }) => {
+    const url = '/modulos/cons/alumnos/kardex.php';
+    const res = await apiClient.get(url);
+    const $ = cheerio.load(res.data);
+    const student = mapTable($, $('body > table:nth-child(2) > tbody'));
+
     response.status = 200;
+    response.body = {
+      student,
+    };
   });
 
   return router.routes();
