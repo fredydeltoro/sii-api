01974259ee093d580cef0c5233f0d3e5f60c6c42
Freddy Monroy <fredydeltoro@gmail.com>
2021-10-16 22:43:35 -0500

    Usar el dominio correcto para el sii

--- src/drivers/apiClient.js
+++ src/drivers/apiClient.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const axiosInstance = () =>
   axios.create({
-    baseURL: 'http://187.216.132.62:81/sistema',
+    baseURL: 'https://sii.itsanjuan.edu.mx/sistema',
     timeout: 1000,
     responseType: 'arraybuffer',
     transformResponse: [(data) => new TextDecoder('latin1').decode(data)],
