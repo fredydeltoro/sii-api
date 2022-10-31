7781b0f6f8f31ddf7af349fe5d6de13bfd61615a
Freddy Monroy <fredydeltoro@gmail.com>
2022-10-30 18:15:01 -0600

    Current notes handle summer period

--- src/routes/current-notes/current-notes.js
+++ src/routes/current-notes/current-notes.js
@@ -6,9 +6,17 @@ const currentNotes = require('../../models/currentNotes.js');
 module.exports = (Router) => {
   const router = new Router();
 
-  router.get('/', async ({ response, token }) => {
+  router.get('/', async ({ response, request, token }) => {
     const url = '/modulos/alu/cons/calif_parciales.php';
-    const res = await apiClient.get(url, token);
+    const query = request.query;
+    let period = '';
+
+    if (query && query.summer) {
+      const date = new Date();
+      period = `?periodo=${date.getFullYear()}`;
+    }
+
+    const res = await apiClient.get(`${url}${period}`, token);
 
     if (res.error) {
       response.status = 401;
